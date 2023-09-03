464e6dee8ffed023c00e94d511f0114b3e3867a0
Daniel Czeschner <daniel.czeschner@web.de>
2023-09-03 11:09:19 +0200

    Setup electron with typescript & live-reload

---
+++ copy.js
@@ -0,0 +1,17 @@
+const copyfiles = require("copyfiles");
+
+// Specify the source directory and destination directory
+const srcDir = "src";
+const destDir = "dist";
+
+// Use a wildcard pattern to match HTML and CSS files recursively
+const pattern = srcDir + "/**/*.{html,css}";
+
+// Copy the matched files to the destination directory
+copyfiles([pattern, destDir], { up: 1 }, (err) => {
+  if (err) {
+    console.error("Error copying files:", err);
+  } else {
+    console.log("Files copied successfully!");
+  }
+});

9e072888110179c6200f73ed0a74d8696d80bc64
Daniel Czeschner <daniel.czeschner@web.de>
2023-09-06 12:07:21 +0200

    Setup of quad tree

--- src/app/core/models/Planet.ts
+++ src/app/core/models/Planet.ts
@@ -1,5 +1,9 @@
-class Planet {
-  public constructor() {}
+import { Circle } from '@timohausmann/quadtree-ts';
+
+class Planet extends Circle {
+  public constructor(x: number, y: number) {
+    super({ x: x, y: y, r: 5 });
+  }
 }
 
 export { Planet };
--- src/app/core/models/Universe.ts
+++ src/app/core/models/Universe.ts
@@ -1,16 +1,24 @@
 import { Planet } from './Planet';
+import { Quadtree } from '@timohausmann/quadtree-ts';
 
 class Universe {
   private canvas: HTMLCanvasElement;
   private context: CanvasRenderingContext2D;
   private planets: Planet[];
+  private tree: Quadtree<Planet>;
 
   public constructor(canvas: HTMLCanvasElement) {
     this.canvas = canvas;
     this.context = this.canvas.getContext('2d');
   }
 
-  public init(): void {}
+  public init(): void {
+    this.tree = new Quadtree({
+      height: 5000,
+      width: 5000,
+      maxObjects: 5,
+    });
+  }
 }
 
 export { Universe };

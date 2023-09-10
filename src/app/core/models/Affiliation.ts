5299fcfcca52a3fd4a76e3c2ca183d66c6c6a63a
Daniel Czeschner <daniel.czeschner@web.de>
2023-09-11 00:15:55 +0200

    Updated database and added affiliation object

---
+++ src/app/core/models/Affiliation.ts
@@ -0,0 +1,11 @@
+class Affiliation {
+  private name: string;
+  private color: string;
+
+  public constructor(name: string, color: string) {
+    this.name = name;
+    this.color = color;
+  }
+}
+
+export { Affiliation };

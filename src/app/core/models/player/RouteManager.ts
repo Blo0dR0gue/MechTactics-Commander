36759c2fb907ded2fee15b4eb21231309b8911d1
Daniel Czeschner <daniel.czeschner@web.de>
2023-09-18 18:21:34 +0200

    Added route calculation between multiple planets

--- src/app/core/models/player/RouteManager.ts
+++ src/app/core/models/player/RouteManager.ts
@@ -16,6 +16,22 @@ class RouteManager {
     this.route = [];
   }
 
+  public calculateRoute(jumpRange: number): void {
+    if (this.targetPlanets.length < 2) return;
+    this.route = [];
+
+    for (let i = 0; i < this.targetPlanets.length - 1; i++) {
+      const p1 = this.targetPlanets[i];
+      const p2 = this.targetPlanets[i + 1];
+      const route = this.findRoute(p1, p2, jumpRange);
+      this.route.concat(route);
+    }
+  }
+
+  public getRoute(): Planet[] {
+    return this.route;
+  }
+
   /**
    * Calculates the shortest route from planet a to planet b
    *

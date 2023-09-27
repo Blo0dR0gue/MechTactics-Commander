0116c55bc7bd739c9c273f69c7c9155f8d104f6f
Daniel Czeschner <daniel.czeschner@web.de>
2023-09-27 19:40:32 +0200

    Added action bar handler and test content for the first 2 tabs

--- src/app/core/index.ts
+++ src/app/core/index.ts
@@ -1,7 +1,10 @@
+import { ActionBarHandler } from './models/map/ActionBarHandler';
 import { Universe } from './models/map/Universe';
 
 const canvasElement = document.getElementById('universe') as HTMLCanvasElement;
 
 const universe = new Universe(canvasElement);
+const actionBarHandler = new ActionBarHandler();
 
 universe.init();
+actionBarHandler.init();

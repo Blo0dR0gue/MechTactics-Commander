baf39238a78aa387b7e65e4c1f1ba15dd7feddf2
Daniel Czeschner <daniel.czeschner@web.de>
2023-09-28 18:30:13 +0200

    Added bootstrap

--- src/app/core/index.ts
+++ src/app/core/index.ts
@@ -1,5 +1,10 @@
 import { ActionBarHandler } from './models/map/ActionBarHandler';
 import { Universe } from './models/map/Universe';
+// Import custom CSS to load bootstrap and override variables
+import './styles/main.scss';
+
+// Import all of Bootstrap's JS
+// import * as bootstrap from 'bootstrap';
 
 const canvasElement = document.getElementById('universe') as HTMLCanvasElement;
 

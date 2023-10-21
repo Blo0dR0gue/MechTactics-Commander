765b3cee6b2c96376debd5b01f0e59bfbca24ecc
Daniel Czeschner <daniel.czeschner@web.de>
2023-10-21 22:05:39 +0200

    Replaced dummy 0.0.9 actions

--- src/app/core/upgrades/Upgrade009.ts
+++ src/app/core/upgrades/Upgrade009.ts
@@ -6,19 +6,8 @@ class Upgrade009 extends AppUpgradeInfo {
   public constructor(config: CoreConfig, database: sqlite3.Database) {
     super(config, database, '0.0.9', 'Test');
     this.actions.push(async () => {
-      await new Promise<void>((resolve) => {
-        setTimeout(() => {
-          resolve();
-        }, 10000);
-      });
-    });
-    this.actions.push(async () => {
-      await new Promise<void>((resolve) => {
-        console.log('test');
-        setTimeout(() => {
-          resolve();
-        }, 5000);
-      });
+      console.log('Upgrading config');
+      this.config.set('version', '0.0.9');
     });
   }
 }

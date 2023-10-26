509845924941fa626dd4707d16a01214206f4fbc
Daniel Czeschner <daniel.czeschner@web.de>
2023-10-26 22:00:58 +0200

    TODOs

--- src/app/core/upgrades/Upgrade009.ts
+++ src/app/core/upgrades/Upgrade009.ts
@@ -9,6 +9,19 @@ class Upgrade009 extends AppUpgradeInfo {
       console.log('Upgrading config');
       this.config.set('version', '0.0.9');
     });
+    this.actions.push(async () => {
+      console.log('Upgrading Database');
+      // TODO: Enable foreign_keys globally -> db.get("PRAGMA foreign_keys = ON")
+      database.serialize(() => {});
+      /*
+      FIX DATABASE LIKE THIS:
+      PRAGMA foreign_keys = 1;
+      CREATE TABLE IF NOT EXISTS Parent(id INTEGER PRIMARY KEY AUTOINCREMENT, name text);
+      CREATE TABLE IF NOT EXISTS Child(id INTEGER PRIMARY KEY AUTOINCREMENT, name text, parent_id INTEGER, FOREIGN KEY (parent_id) REFERENCES Parent (id));
+      INSERT INTO Child (name, parent_id) VALUES('test',1);
+      INSERT INTO Child (name, parent_id) VALUES('test',77);
+      */
+    });
   }
 }
 

37b5ba28fc14d702b31105fe61d1e335db553285
Daniel Czeschner <daniel.czeschner@web.de>
2023-10-29 20:04:46 +0100

    Removed comment

--- src/app/core/upgrades/Upgrade009.ts
+++ src/app/core/upgrades/Upgrade009.ts
@@ -76,15 +76,6 @@ class Upgrade009 extends AppUpgradeInfo {
       console.log('Dropping old tables');
       await database.exec('DROP TABLE Planet_old;');
       await database.exec('DROP TABLE Affiliation_old;');
-
-      /*
-      FIX DATABASE LIKE THIS:
-      PRAGMA foreign_keys = 1;
-      CREATE TABLE IF NOT EXISTS Parent(id INTEGER PRIMARY KEY AUTOINCREMENT, name text);
-      CREATE TABLE IF NOT EXISTS Child(id INTEGER PRIMARY KEY AUTOINCREMENT, name text, parent_id INTEGER, FOREIGN KEY (parent_id) REFERENCES Parent (id));
-      INSERT INTO Child (name, parent_id) VALUES('test',1);
-      INSERT INTO Child (name, parent_id) VALUES('test',77);
-      */
     });
   }
 }

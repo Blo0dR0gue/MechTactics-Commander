3d615cbd528b1048dea8f037fe280f93b61799de
Daniel Czeschner <daniel.czeschner@web.de>
2023-10-29 15:29:25 +0100

    Upgrade database

--- src/app/core/upgrades/Upgrade009.ts
+++ src/app/core/upgrades/Upgrade009.ts
@@ -34,6 +34,44 @@ class Upgrade009 extends AppUpgradeInfo {
       // Insert current battletech map version
       await database.exec('INSERT INTO UniverseAge (age) VALUES ("3025")');
 
+      // Add all affiliation objects
+      await database.each(
+        'SELECT name, color FROM Affiliation_old;',
+        async (err, data: { name: string; color: string }) => {
+          if (err) {
+            console.log(err);
+            return;
+          }
+          await database.exec(
+            `INSERT INTO Affiliation (name, color) VALUES ("${data.name}", "${data.color}")`
+          );
+        }
+      );
+
+      const planetAffilationData = (await database.all(
+        'SELECT p.name as planetName, link, x, y, a.name as affiliationName FROM Planet_old as p JOIN Affiliation_old as a ON p.affiliation = a.rowid;'
+      )) as {
+        planetName: string;
+        link: string;
+        x: number;
+        y: number;
+        affiliationName: string;
+      }[];
+
+      for (const data of planetAffilationData) {
+        await database.exec(
+          `INSERT INTO Planet (name, link, x, y) VALUES ("${data.planetName}", "${data.link}", ${data.x}, ${data.y});`
+        );
+
+        await database.exec(
+          `INSERT INTO PlanetAffiliationAge (affiliationID, universeAgeID, planetID) VALUES ( (SELECT id FROM Affiliation WHERE name = "${data.affiliationName}"), 1, (SELECT id FROM Planet WHERE name = "${data.planetName}"));`
+        );
+      }
+
+      // Drop old tables
+      await database.exec('DROP TABLE Planet_old;');
+      await database.exec('DROP TABLE Affiliation_old;');
+
       /*
       FIX DATABASE LIKE THIS:
       PRAGMA foreign_keys = 1;

0a7956f6ab606818f54613b5be332454782733fa
Daniel Czeschner <daniel.czeschner@web.de>
2023-12-19 11:18:58 +0100

    fix: Update type to not return functions props

--- src/app/types/UtilityTypes.ts
+++ src/app/types/UtilityTypes.ts
@@ -10,7 +10,9 @@ type ObjectWithKeys = Record<string, unknown>;
  * {name: 'test', coord: {x: 1, y: 1}} => 'name' | 'text' | 'coord' | 'coord.x' | 'coord.y'
  */
 type ObjectPropsRec<T, U extends keyof T = keyof T> = U extends string
-  ? T[U] extends object
+  ? T[U] extends (...args: unknown[]) => unknown
+    ? never
+    : T[U] extends object
     ? U | `${U & string}.${ObjectPropsRec<T[U]>}`
     : U
   : never;

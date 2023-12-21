c00bdb1134c6765fda50d37826734d46bd4106bd
Daniel Czeschner <daniel.czeschner@web.de>
2023-12-21 12:30:53 +0100

    feat: Added loading indicator

---
+++ src/app/renderer/utils/RingLoadingIndicator.ts
@@ -0,0 +1,37 @@
+class RingLoadingIndicator {
+  private loader: HTMLDivElement;
+
+  constructor(
+    private parentElement: HTMLElement,
+    private style: 'lds-ring-light' | 'lds-ring-dark' = 'lds-ring-light'
+  ) {
+    this.loader = this.createLoader();
+  }
+
+  createLoader(): HTMLDivElement {
+    const holder = document.createElement('div');
+    holder.className = 'position-absolute top-50 start-50 translate-middle';
+
+    const loader = document.createElement('div');
+    loader.classList.add('lds-ring');
+    loader.classList.add(this.style);
+    for (let i = 0; i < 4; i++) {
+      const div = document.createElement('div');
+      loader.appendChild(div);
+    }
+    holder.appendChild(loader);
+    return holder;
+  }
+
+  show() {
+    this.parentElement.appendChild(this.loader);
+  }
+
+  hide() {
+    if (this.loader.parentNode === this.parentElement) {
+      this.parentElement.removeChild(this.loader);
+    }
+  }
+}
+
+export { RingLoadingIndicator };

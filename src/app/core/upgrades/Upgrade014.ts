a530847b175c7e5aae46c53186f38320ef3ebf18
Daniel Czeschner <daniel.czeschner@web.de>
2024-03-22 16:24:50 +0100

    chore: Upgrade title

--- src/app/core/upgrades/Upgrade014.ts
+++ src/app/core/upgrades/Upgrade014.ts
@@ -4,10 +4,7 @@ import { CoreConfig } from '../CoreConfig';
 
 class Upgrade014 extends AppUpgradeInfo {
   public constructor(config: CoreConfig, database: Database) {
-    super(config, database, '0.1.4', 'TODO');
-    this.actions.push(async () => {
-      // TODO:
-    });
+    super(config, database, '0.1.4', 'Import/Export of database tables');
   }
 }
 

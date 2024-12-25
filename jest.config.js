95316f7d1ff206b30102af7f55e0b609885724bf
Daniel Czeschner <daniel.czeschner@web.de>
2024-12-25 20:29:36 +0100

    feat: Added test tsconfig

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,13 @@
 module.exports = {
-  transform: { '^.+\\.ts?$': 'ts-jest' },
+  transform: {
+    '^.+\\.ts$': [
+      'ts-jest',
+      {
+        tsconfig: './tsconfig.test.json'
+      }
+    ]
+  },
   testEnvironment: 'node',
   testRegex: 'src/test/.*\\.(test|spec)?\\.(ts|tsx)$',
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node']
 };

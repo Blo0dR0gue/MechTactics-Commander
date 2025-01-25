64a9d22a4b2fe4e621ee90bb999c384ae2b0fddc
Daniel Czeschner <daniel.czeschner@web.de>
2025-01-25 22:54:07 +0100

    refactor: PlanetTagRepository does not need to use id anymore

--- src/app/core/repositories/PlanetTagRepository.ts
+++ src/app/core/repositories/PlanetTagRepository.ts
@@ -1,12 +1,13 @@
 import { Database } from 'sqlite';
 import { BaseRepository } from './BaseRepository';
-import { ForcefullyOmit } from '../../types/UtilityTypes';
 import { PlanetTag } from '../../types/PlanetData';
+import { ForcefullyOmit } from '../../types/UtilityTypes';
+import { stringToSnakeCase } from '../../renderer/utils/Utils';
 
 export class PlanetTagRepository extends BaseRepository<
   PlanetTag,
-  { id: number },
-  { id: number }
+  { planetID: number; tagKey: string },
+  undefined
 > {
   public constructor(database: Database) {
     super(database, 'PlanetTag');
@@ -18,25 +19,42 @@ export class PlanetTagRepository extends BaseRepository<
     ]);
   }
 
-  public async createMany(
-    tagList: ForcefullyOmit<PlanetTag, 'id'>[]
-  ): Promise<PlanetTag[]> {
+  public create(data: ForcefullyOmit<PlanetTag, never>): Promise<number> {
+    const createData = this.convertToDatabaseFormat(data);
+
+    return super.create(createData);
+  }
+
+  public createOrReplace(data: PlanetTag): Promise<number | null> {
+    const createData = this.convertToDatabaseFormat(data);
+    return super.createOrReplace(createData);
+  }
+
+  public updateByKey(): Promise<boolean> {
+    throw new Error(`Can't update a PlanetTag!`);
+  }
+
+  public async createMany(tagList: PlanetTag[]): Promise<PlanetTag[]> {
     const results: PlanetTag[] = [];
 
     const insertTag = await this.database.prepare(
       `INSERT INTO PlanetTag (planetID, tagKey, tagValue) VALUES (?, ?, ?);`
     );
     for (const tag of tagList) {
-      const result = await insertTag.run(
-        tag.planetID,
-        tag.tagKey,
-        tag.tagValue
-      );
-      results.push({ ...tag, id: result.lastID });
+      await insertTag.run(tag.planetID, tag.tagKey, tag.tagValue);
+      results.push({ ...tag });
     }
 
     await insertTag.finalize();
 
     return results;
+  }
+
+  private convertToDatabaseFormat(data: PlanetTag) {
+    const { tagKey, ...rest } = data;
+
+    const snakeCaseTagKey = stringToSnakeCase(tagKey);
+
+    return { tagKey: snakeCaseTagKey, ...rest };
   }
 }

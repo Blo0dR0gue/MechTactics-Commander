65e59c4369e88a6973f0d2598e5f6b70155929aa
Daniel Czeschner <daniel.czeschner@web.de>
2025-01-25 22:53:38 +0100

    refactor: planetaffiliationage repository should use the base repository

--- src/app/core/repositories/PlanetAffiliationAgeRepository.ts
+++ src/app/core/repositories/PlanetAffiliationAgeRepository.ts
@@ -1,29 +1,26 @@
 import { Database } from 'sqlite';
 import {
   PlanetAffiliationAgeData,
-  PlanetAffiliationAgeWithNamesData,
+  PlanetAffiliationAgeWithNamesData
 } from '../../types/PlanetAffiliationAge';
+import { BaseRepository } from './BaseRepository';
 
-export class PlanetAffiliationAgeRepository {
-  protected database: Database;
-
+export class PlanetAffiliationAgeRepository extends BaseRepository<
+  PlanetAffiliationAgeData,
+  { universeAge: number; planetID: number; affiliationID: number },
+  object
+> {
   public constructor(database: Database) {
-    this.database = database;
-  }
-
-  public getAll(): Promise<PlanetAffiliationAgeData[]> {
-    return this.database.all(
-      'SELECT planetID, affiliationID, universeAge, planetText FROM PlanetAffiliationAge;'
-    );
+    super(database, 'PlanetAffiliationAge');
   }
 
   public getAllWithNames(): Promise<PlanetAffiliationAgeWithNamesData[]> {
     return this.database.all(
-      'SELECT planetID, affiliationID, universeAge, planetText, p.name as planetName, a.name as affiliationName FROM PlanetAffiliationAge as u JOIN Planet as p ON p.id = u.planetID JOIN Affiliation as a ON a.id = u.affiliationID;'
+      'SELECT u.*, p.name as planetName, a.name as affiliationName FROM PlanetAffiliationAge as u JOIN Planet as p ON p.id = u.planetID JOIN Affiliation as a ON a.id = u.affiliationID;'
     );
   }
 
-  public getAllUniverseAges(): Promise<{ universeAge: number }[]> {
+  public getAllUniverseAges(): Promise<Set<number>> {
     return this.database
       .all(`SELECT DISTINCT universeAge FROM PlanetAffiliationAge;`)
       .then((data) => {
@@ -32,26 +29,6 @@ export class PlanetAffiliationAgeRepository {
           return acc;
         }, new Set<number>());
       });
-  }
-
-  public async create(data: PlanetAffiliationAgeData): Promise<void> {
-    await this.database.run(
-      'INSERT INTO PlanetAffiliationAge (universeAge, planetID, affiliationID, planetText) VALUES (?, ?, ?, ?);',
-      data.universeAge,
-      data.planetID,
-      data.affiliationID,
-      data.planetText
-    );
-    return undefined;
-  }
-
-  public async delete(data: Omit<PlanetAffiliationAgeData, 'planetText'>) {
-    return this.database.run(
-      'DELETE FROM PlanetAffiliationAge WHERE planetID = ? AND universeAge = ? AND affiliationID = ?;',
-      data.planetID,
-      data.universeAge,
-      data.affiliationID
-    );
   }
 
   public async deleteAllByPlanet(planetID: number): Promise<void> {
@@ -67,16 +44,6 @@ export class PlanetAffiliationAgeRepository {
     await this.database.run(
       'UPDATE PlanetAffiliationAge SET affiliationID = 0 WHERE affiliationID = ?;',
       [affiliationID]
-    );
-  }
-
-  public async update(data: PlanetAffiliationAgeData): Promise<void> {
-    await this.database.run(
-      'UPDATE PlanetAffiliationAge SET affiliationID = ?, planetText = ? WHERE planetID = ? AND universeAge = ?;',
-      data.affiliationID,
-      data.planetText,
-      data.planetID,
-      data.universeAge
     );
   }
 }
